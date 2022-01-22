2e4acebaac01c844731e3f71d7dfa8fd3a444836
Paul Keating <palisar@hotmail.co.uk>
2022-01-22 21:10:26 +0000

    Added a gameboard to control the game rules and flow

--- ZombieSurvivorsKata/GameBoard.cs
+++ ZombieSurvivorsKata/GameBoard.cs
@@ -10,5 +10,40 @@ namespace ZombieSurvivorsKata
 {
     public class GameBoard
     {
+        public GameBoard()
+        {
+            Survivors = new List<Survivor>();
+            SurvivorsSpawned = false;
+        }
+
+        public List<Survivor> Survivors { get; set; }
+        public bool SurvivorsSpawned { get; set; }
+        public bool GameOverFlag { get; set; }
+
+        public Survivor AddSurvivor(string name)
+        {
+            var survivor = Survivors.Select(s => s.Name == name);
+            if (survivor is null)
+            {
+                Survivors.Add(new Survivor(name));
+            }
+
+            SurvivorsSpawned = true;
+
+            return Survivors.Last();
+        }
+
+        public void RemoveSurvivor(Survivor survivor)
+        {
+            Survivors.Remove(survivor);
+            GameOverFlag = IsGameOver();
+        }
+
+        public bool IsGameOver()
+        {
+            return SurvivorsSpawned && Survivors.Count < 1;
+        }
+
+
     }
 }
