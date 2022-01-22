fcefbafb0056541c1dd729e00617cf22464910a2
Paul Keating <palisar@hotmail.co.uk>
2022-01-22 21:10:03 +0000

    Added an equipment class, needs to be fleshed out in future iterations

---
+++ ZombieSurvivorsKata/Equipment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZombieSurvivorsKata
+{
+    public class Equipment
+    {
+        public Equipment(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+    }
+}
