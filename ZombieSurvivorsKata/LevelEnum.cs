b0838df6a582d017d1feec63301ad32f7eb1ff0c
Paul Keating <palisar@hotmail.co.uk>
2022-01-27 16:13:33 +0000

    Added a Level enum for the players and game to progress

---
+++ ZombieSurvivorsKata/LevelEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZombieSurvivorsKata
+{
+    public enum Level
+    {
+        Blue = 0,
+        Yellow = 7,
+        Orange = 19,
+        Red = 42,
+    }
+}
